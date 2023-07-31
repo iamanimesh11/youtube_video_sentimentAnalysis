74caf576caf167004199369934d11e07e8f789db
iamanimesh11 <122896955+iamanimesh11@users.noreply.github.com>
2023-07-31 12:21:17 +0530

    Update ytapp.py

--- ytapp.py
+++ ytapp.py
@@ -15,7 +15,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
 
-st.set_page_config(page_title='Youtube sentiment Analysis', layout='wide',page_icon=":yt:")
+st.set_page_config(page_title='Youtube sentiment Analysis', layout='wide',page_icon="▶️")
 
 hide_github_icon_js = """
 <style>
